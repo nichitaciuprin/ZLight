cb4693acef0b4a8abf50719ebf4133f7259d1905
Nichita Ciuprin <nichitaciuprin@gmail.com>
2025-06-24 14:17:42 +0300

    Moved camera functions out of ZLight.h

--- tests/ZLightTest/main.cpp
+++ tests/ZLightTest/main.cpp
@@ -8,6 +8,114 @@
 Camera cam = { 0, 1.70f, 0 };
 Vector3 light = { 0, 1, 0 };
 
+static inline Vector3 CameraGetAxisX(Camera* camera)
+{
+    Vector3 result = { 1, 0, 0 };
+    result = Vector3RotateX(result, camera->pitch);
+    result = Vector3RotateY(result, -camera->yaw);
+    return result;
+}
+static inline Vector3 CameraGetAxisY(Camera* camera)
+{
+    Vector3 result = { 0, 1, 0 };
+    result = Vector3RotateX(result, camera->pitch);
+    result = Vector3RotateY(result, -camera->yaw);
+    return result;
+}
+static inline Vector3 CameraGetAxisZ(Camera* camera)
+{
+    Vector3 result = { 0, 0, 1 };
+    result = Vector3RotateX(result, camera->pitch);
+    result = Vector3RotateY(result, -camera->yaw);
+    return result;
+}
+
+static inline void CameraRotateClamped(Camera* camera, Vector2 rot)
+{
+    camera->yaw    += rot.x;
+    camera->pitch  += rot.y;
+
+    float angle = 90;
+    // float angle = 100; // 100 feels better
+
+    camera->yaw = MathMod(camera->yaw, MATH_PI_MUL_2);
+    camera->pitch = MathClamp(camera->pitch, MathToRadians(-angle), MathToRadians(angle));
+}
+static inline void CameraMoveFree(Camera* camera, Vector3 move)
+{
+    // TODO double check it works
+
+    Vector3 x = CameraGetAxisX(camera);
+    Vector3 y = CameraGetAxisY(camera);
+    Vector3 z = CameraGetAxisZ(camera);
+
+    x = Vector3Mul(x, move.x);
+    y = Vector3Mul(y, move.y);
+    z = Vector3Mul(z, move.z);
+    // x.x *= move.x; x.y *= move.x; x.z *= move.x;
+    // y.x *= move.y; y.y *= move.y; y.z *= move.y;
+    // z.x *= move.z; z.y *= move.z; z.z *= move.z;
+
+    camera->pos = Vector3Add(camera->pos, z);
+    camera->pos = Vector3Add(camera->pos, y);
+    camera->pos = Vector3Add(camera->pos, x);
+}
+static inline void CameraMovePlane(Camera* camera, Vector2 move)
+{
+    Vector3 up = { 0, 1, 0 };
+    Vector3 forward = { 0, 0, 1 };
+
+    forward = Vector3RotateY(forward, -camera->yaw);
+
+    Vector3 right = Vector3Cross(up, forward);
+
+    forward.x *= move.y;
+    forward.z *= move.y;
+    right.x *= move.x;
+    right.z *= move.x;
+
+    camera->pos = Vector3Add(camera->pos, forward);
+    camera->pos = Vector3Add(camera->pos, right);
+}
+
+static inline void CameraInputRotateClamped(Camera* camera, float distance, bool left, bool up, bool down, bool right)
+{
+    int x = (int)right - (int)left;
+    int y = (int)up - (int)down;
+
+    Vector2 rot = { (float)x, (float)y };
+
+    rot = Vector2Normalize(rot);
+    rot = Vector2Mul(rot, distance);
+
+    CameraRotateClamped(camera, rot);
+}
+static inline void CameraInputMoveFree(Camera* camera, float distance, bool w, bool a, bool s, bool d, bool e, bool q)
+{
+    int x = (int)d - (int)a;
+    int y = (int)e - (int)q;
+    int z = (int)w - (int)s;
+
+    Vector3 move = { (float)x, (float)y, (float)z };
+
+    move = Vector3Normalize(move);
+    move = Vector3Mul(move, distance);
+
+    CameraMoveFree(camera, move);
+}
+static inline void CameraInputMovePlane(Camera* camera, float distance, bool w, bool a, bool s, bool d)
+{
+    int x = (int)d - (int)a;
+    int y = (int)w - (int)s;
+
+    Vector2 move = { (float)x, (float)y };
+
+    move = Vector2Normalize(move);
+    move = Vector2Mul(move, distance);
+
+    CameraMovePlane(camera, move);
+}
+
 void UpdatePlayerCamera(Camera* camera, SysWindow* window, float delta)
 {
     auto w = SysWindowKeyDown(window, 'W');
@@ -25,7 +133,6 @@ void UpdatePlayerCamera(Camera* camera, SysWindow* window, float delta)
     int x, y; SysWindowGetMouseDelta(window, &x, &y);
     CameraRotateClamped(camera, { x*0.01f, y*0.01f });
 }
-
 void DrawFunc(Bitmap* bitmap)
 {
     // BitmapExtDrawGrid(bitmap);
