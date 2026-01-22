1eb93050d162d3547f50da085df2cbcbfe39ec5f
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-01-22 17:09:19 +0200

    UPDATE

---
+++ tests/Voxel3/build.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+flags="$flags -Werror -Wall"
+flags="$flags -std=c99 -m64"
+flags="$flags -g0 -O3 -D NDEBUG"
+
+rm -rf ./build
+mkdir  ./build
+
+gcc ./main.c $src -o ./build/main $flags -I../.. -lX11 -lm
